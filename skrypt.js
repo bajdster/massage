cf088b30731350c53e5f43ffccd72f06fe37a288
Michał <38719617+bajdster@users.noreply.github.com>
2018-11-08 20:01:27 +0100

    Add files via upload

--- skrypt.js
+++ skrypt.js
@@ -1,13 +1,13 @@
 $(document).ready(function()
-{		
+{
 	//Napis powitania wyswietla sie po 2 sek
 	//$(".powitanie").fadeIn(2000);
-	
+
 	//Otwieranie się menu po kliknieciu w logo lotos lub napis "Menu"
 	$("#menuLotos, .napisMenu").click(function()
 	{
 		$(".elementyNawigacji").slideToggle(500);//nie wiadomo czemu slideToggle itp nie działaja
-		//przez wersję slim JQ nie działy funkcje slideToggle itp
+		//RE: przez wersję slim JQ nie działy funkcje slideToggle itp
 	});
 	//klikniecie w ikony masazu i odczulania otwieraja tekst oraz daja obramowanie szare
 	$(".ikonaMasaz").click(function()
@@ -15,21 +15,21 @@ $(document).ready(function()
 		$(".opisUslugiMasaz").slideToggle(500)
 		$(".ikonaMasaz").toggleClass("activeClass");
 	});
-	
+
 	$(".ikonaAlergia").click(function()
 	{
 		$(".opisUslugiOdczulanie").slideToggle(500)
 		$(".ikonaAlergia").toggleClass("activeClass");
+
 	});
-	
+
 	//klikniecie na div cennik otwiera elementy cennika
 	$("#cennik").click(function()
 	{
 		$(".masazeCennik, .alergiaCennik, .odczulanieCennik").slideToggle(500)
 		$("#strzalkaDol").toggleClass("obrotStrzalki");
 	});
-	
-	
+
 	//Klikniecie przycisku "do gory" przewija strone z animacja 800ms
 	$("#przyciskDoGory").click(function()
 	{
@@ -38,13 +38,13 @@ $(document).ready(function()
 				scrollTop: 0
 			}, 800);
 	});
-	
+
 	//Przewiniecie strony pokazuje przycisk "do gory"
 	$(window).scroll(function()
 	{
 		var przesuniecie = window.pageYOffset;
 		var przycisk = $("#przyciskDoGory");
-		
+
 		if(przesuniecie>300)
 		{
 			przycisk.css("display", "block");
@@ -54,5 +54,6 @@ $(document).ready(function()
 			przycisk.css("display", "none");
 		}
 	});
-	
+
+
 });
